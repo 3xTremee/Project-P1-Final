781aae3e975e6f416397b0c7d92294966e8d86dc
Jakob Strøm <Jakzus@gmail.com>
2023-11-17 11:27:39 +0100

    Timestamp indtil videre

--- timestamp.c
+++ timestamp.c
@@ -1,1 +1,31 @@
 #include "timestamp.h"
+
+/*
+Add item to array/object.
+CJSON_PUBLIC(cJSON_bool) cJSON_AddItemToArray(cJSON *array, cJSON *item)
+{
+    return add_item_to_array(array, item);
+}
+
+*/
+
+void timestamp_administration() {
+
+    char choice;
+    printf("Has the medicine been administered?\n\nType 'Y' for yes or 'N' no \n>");
+
+//Kode som tjekker om valg er Y eller N
+    scanf(" %c", &choice);
+    choice = toupper(choice);
+
+    if (choice == 'Y') {
+        time_t t;
+        time(&t);
+        char adm[100];
+        sprintf(adm, "Time of administration: %s", ctime(&t));
+
+        cJSON_AddItemToArray( , adm) /* Hvordan får jeg fat i det rigtige cjson array*/
+
+    }
+
+}
