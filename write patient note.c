93c0e1c51f1fe473172871fc0cfcbfbaa3d92b4d
tobi7602 <tkoch23@student.aau.dk>
2023-11-20 12:13:18 +0100

    add initial write patient notes

---
+++ write patient note.c
@@ -0,0 +1,71 @@
+#include "write patient note.h"
+
+void write_note(){
+    /*
+    char *PatientCPR = "308031234";
+    FILE *fp = fopen("patient_notes.json", "r");
+    if (fp == NULL) {
+        printf("Error: Unable to open the file.\n");
+        return;
+    }
+
+    // This code reads the contents of the opened file into a character buffer named buffer.
+    char buffer[3048];
+    int len = fread(buffer, 1, sizeof(buffer), fp);
+    fclose(fp);
+
+    //Extracts the array from the parsed JSON data
+    cJSON *json = cJSON_Parse(buffer);
+    if (json == NULL) {
+        const char *error_ptr = cJSON_GetErrorPtr();
+        if (error_ptr != NULL) {
+            printf("Error: %s\n", error_ptr);
+        }
+        cJSON_Delete(json);
+        return;
+    }
+
+    bool PatientInFile = 0;
+
+    //To check if the patient is registered in the notes system, check if the given CPR can be read in the file
+    cJSON *patients = cJSON_GetObjectItemCaseSensitive(json, "Patients");
+    if (cJSON_IsArray(patients)) {
+        for (int i = 0; i < cJSON_GetArraySize(patients); i++) {
+            cJSON *ReadingPatient = cJSON_GetArrayItem(patients, i);
+            cJSON *cpr = cJSON_GetObjectItemCaseSensitive(ReadingPatient, "CPR");
+
+
+            //The atof funktion converts the array of chars (String) into a float
+            if (cJSON_IsNumber(cpr) && (cpr->valuedouble == atof(PatientCPR))) {
+                cJSON *cpr = cJSON_GetObjectItemCaseSensitive(patients, "cpr");
+
+                if (cJSON_IsNumber(cpr) && (patients->valuestring != NULL)) {
+                    PatientInFile = 1;
+                    break;
+                }
+                }
+            }
+        } else{
+        printf("Error: 'Patients' is not an array in the JSON.\n");
+    }
+
+    //Scan patient note
+    char PatientNote[100];
+    printf("Please type error or note:\n");
+    scanf("%s\n", PatientNote);*/
+
+    //Open JSON file in 'W' ?? eller skal den åbnes efter of skrive "buffer" til den? det skal den nok...
+
+    //create object for the CPR if PatientInFIle == 0
+    //CJSON_PUBLIC(cJSON *) cJSON_CreateObject(void); - ????
+    //if (PatientInFile == 0){
+    //    cJSON_CreateObject();
+    //}
+
+    //create item in object (CPR)
+    //CJSON_PUBLIC(cJSON_bool) cJSON_AddItemToObject(cJSON *object, const char *string, cJSON *item);
+    //item = current time
+    //string = scanned note
+    //object = PatientCPR
+}
+
---
+++ write patient note.h
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "cJSON.h"
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+
+void write_note();
+
+#ifndef PROJECT_P1_FINAL_WRITE_PATIENT_NOTE_H
+#define PROJECT_P1_FINAL_WRITE_PATIENT_NOTE_H
+
+#endif //PROJECT_P1_FINAL_WRITE_PATIENT_NOTE_H
