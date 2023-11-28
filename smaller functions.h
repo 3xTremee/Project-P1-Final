df28f452c138918e1f68d6e8e38e00626355d89a
Jakob Strøm <Jakzus@gmail.com>
2023-11-28 11:15:02 +0100

    New functions

--- smaller functions.h
+++ smaller functions.h
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void file_opening(FILE *fp);
+void check_file_opening(FILE *fp);
+long size_of_file(FILE *fp);
+void check_buffer(char *buffer);
 void weightBasedDosage();
 void liquidDosage(int unitChoice2);
